058f5623f93ad106a585ed15bcdd856c48660300
lejonu <leonardo.ferax@gmail.com>
2020-04-01 14:29:24 -0300

    Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
-import axios from 'axios'
+import Search from './models/Search'
 
-async function getResults( query ){    
-    
-    try {
-        const res = await axios( `https://forkify-api.herokuapp.com/api/search?q=${ query }` )
+const search = new Search( 'pizza' )
 
-        const recipes = res.data.recipes
-    
-        console.log( recipes )
-    } catch ( error ) {
-        alert( error )
-    }
-
-}
-
-getResults( 'pizza' )
+search.getResults()
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,23 @@
-export default 'I am an export string.'
+import axios from 'axios'
+
+export default class Search {
+    constructor( query ){
+        this.query = query
+        // this.getResults()
+    }
+
+    async getResults(){    
+    
+        try {
+            const res = await axios( `https://forkify-api.herokuapp.com/api/search?q=${ this.query }` )
+    
+            this.result = res.data.recipes
+        
+            // console.log( this.result )
+        } catch ( error ) {
+            alert( error )
+        }
+    }
+
+
+}
