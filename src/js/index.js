0646fb2fab13bf2900fb5c5ce0c9d78e23e33010
lejonu <leonardo.ferax@gmail.com>
2020-04-01 20:56:12 -0300

    Building the Searhc View

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import Search from './models/Search'
-
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
+// elements
 // Global state of the app
 const state = {}
 
 const constrolSearch = async () => {
     // 1. Get the query from the view
-    const query = 'pizza'
+    const query = searchView.getInput()
+    // console.log( query )
 
     if( query ){
         // 2. Create new search object an add to state
@@ -13,18 +16,21 @@ const constrolSearch = async () => {
         // console.log( state.search )
 
         // 3. Prepare UI for results
+        searchView.clearInput()
+        searchView.clearResults()
 
         // 4. Search for recipes
         await state.search.getResults()
 
         // 5. Render results on UI
-        console.log( state.search.result )
+        // console.log( state.search.result )
+        searchView.renderResults( state.search.result )
     }
 
 
 }
 
-document.querySelector( '.search' ).addEventListener( 'submit', e =>{
+elements.searchForm.addEventListener( 'submit', e =>{
     e.preventDefault()
     constrolSearch()
 })
